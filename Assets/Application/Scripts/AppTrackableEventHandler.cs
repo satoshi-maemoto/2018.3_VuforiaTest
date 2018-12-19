12e66fa5dbe568a4bb36f899e72279c2224ace17
satoshi-maemoto <peugeot.106.s16@gmail.com>
2018-12-19 12:38:12 +0900

    first commit.

---
+++ Assets/Application/Scripts/AppTrackableEventHandler.cs
@@ -0,0 +1,32 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class AppTrackableEventHandler : DefaultTrackableEventHandler
+{
+    public TextMeshPro text;
+    private int FoundCount { get; set; } = 0;
+    private int LostCount { get; set; } = 0;
+
+    private void Awake()
+    {
+        this.ShowInformation();
+    }
+
+    protected override void OnTrackingFound()
+    {
+        this.FoundCount++;
+        this.ShowInformation();
+    }
+
+    protected override void OnTrackingLost()
+    {
+        this.LostCount++;
+        this.ShowInformation();
+    }
+
+    private void ShowInformation()
+    {
+        this.text.text = $"Found: {this.FoundCount}\nLost: {this.LostCount}";
+        this.text.GetComponent<Animator>()?.Play("Zoom");
+    }
+}
